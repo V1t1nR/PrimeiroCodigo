b2d34d7210d24114540d8a985cf712d3c7ab5306
vitorkl <vitormatias.617@hotmail.com>
2023-12-13 14:53:31 -0300

    Calculadora

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    char operador;
+    double num1, num2, resultado;
+
+    // Solicita ao usuário que insira a operação desejada
+    printf("Digite a operacao (+, -, *, /): ");
+    scanf("%c", &operador);
+
+    // Solicita ao usuário que insira dois números
+    printf("Digite dois numeros: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Realiza a operação selecionada
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            // Verifica se o segundo número é diferente de zero para evitar divisão por zero
+            if (num2 != 0) {
+                resultado = num1 / num2;
+            } else {
+                printf("Erro! Divisao por zero nao permitida.\n");
+                return 1;  // Retorna um código de erro
+            }
+            break;
+        default:
+            printf("Operador invalido.\n");
+            return 1;  // Retorna um código de erro
+    }
+
+    // Exibe o resultado
+    printf("Resultado: %.2lf\n", resultado);
+
+    return 0;
+}
